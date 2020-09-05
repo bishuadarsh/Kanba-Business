584a9aa8b496d8864899609ee3383ab5d2aa2bfd
mac <mac@mas-MacBook-Pro.local>
2020-09-05 07:27:05 +0530

    First Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  apiKey: "AIzaSyCv5hTReMvDVX6pDPca-uBDlffl_d-7itU",
+  authDomain: "kanbafood.firebaseapp.com",
+  databaseURL: "https://kanbafood.firebaseio.com",
+  projectId: "kanbafood",
+  storageBucket: "kanbafood.appspot.com",
+  messagingSenderId: "821600460030",
+  appId: "1:821600460030:web:081fb6bc76b800dd5419af",
+  measurementId: "G-QWDVNY1TZ5"
+  }
 };
